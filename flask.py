58a681fbbb4dd64f7df833eeb627506a5622dc7a
aakashrajaraman <20btrcl069@jainuniversity.ac.in>
2023-05-07 00:42:45 +0530

    changed dir names

---
+++ flask.py
@@ -0,0 +1,8 @@
+from flask import Flask, render_template, request, redirect, url_for, session
+import joblib
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template('index.html')
