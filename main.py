64ba682e4fa5a085b57b7c1d2712d7b010e52871
aakashrajaraman <20btrcl069@jainuniversity.ac.in>
2023-05-07 03:30:50 +0530

    updated form and flask structure

--- main.py
+++ main.py
@@ -1,11 +1,29 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 import joblib
 
 app = Flask(__name__)
+rf = joblib.load('Indivolt/rfmodel.joblib')
 
 @app.route('/', methods = ['GET'])
 def index():
-    return render_template('index.html')
+    return render_template('ab.html')
+@app.route('/submit', methods = ['POST'])
+def submit():
+    norooms = request.form['norooms']
+    nopeople = request.form['nopeople']
+    area = request.form['area']
+    acs = request.form['acs']
+    tvs = request.form['tvs']
+    incomes = request.form['income']
+    kids = request.form['kids']
+    actime = request.form['actime']
+    tvtime = request.form['tvtime']
+    arr = [[norooms, nopeople, area, acs, tvs,1, incomes, kids, 1,actime, tvtime]]
+    pred = rf.predict(arr)
+    print(pred)
+    return render_template('ab.html', pred = pred)
+    
+
 
 if __name__ == '__main__':
     app.run(debug=True, port=3000)
