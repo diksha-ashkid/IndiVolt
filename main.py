6fbb15a0a7df5bdb1bedbade8698d933c53f21b9
aakashrajaraman <20btrcl069@jainuniversity.ac.in>
2023-05-07 07:27:53 +0530

    updated routing

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 from flask import Flask, render_template, request
 import joblib
+import numpy as np
+import random
 
 app = Flask(__name__)
 rf = joblib.load('Indivolt/rfmodel.joblib')
@@ -20,8 +22,13 @@ def submit():
     tvtime = request.form['tvtime']
     arr = [[norooms, nopeople, area, acs, tvs,1, incomes, kids, 1,actime, tvtime]]
     pred = rf.predict(arr)
-    pred = str(pred[0])+"rupees"
-    return render_template('ab.html', pred = pred)
+    pred = int(pred[0])
+    i = random.uniform(0.5, 1.5)
+    print(i)
+    rencost  = np.round(pred - (12*i/100)*pred)
+    profit = np.round(int(pred) - rencost)
+    percent = np.round((profit/int(pred))*100)
+    return render_template('dashboard.html', currcost = pred, rencost = rencost, profit = profit, percent = percent)
     
 
 
